f1856b9607cf9c107ec07ec2e93e9eb3e400196f
gabrielfaria <gabrielfca222@gmail.com>
2019-09-03 21:35:41 -0300

    first

---
+++ thread/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace thread
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::thread.Properties.Resources.soccer_ball_icon_11;
+            this.pictureBox2.Location = new System.Drawing.Point(324, 131);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(60, 55);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::thread.Properties.Resources.human_stick_figure_clipart_40644;
+            this.pictureBox1.Location = new System.Drawing.Point(228, 391);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(12, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(920, 441);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 511);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ thread/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace thread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            KeyDown += new KeyEventHandler(Form1_KeyDown);
+            tm.Interval = 500;
+            tm.Tick += new EventHandler(tm_Tick);
+            tm.Start();
+        }
+        Timer tm = new Timer();
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            int x = pictureBox1.Location.X;
+            int y = pictureBox1.Location.Y;
+            int velocidade=9;
+
+            if (e.KeyCode == Keys.Right) {
+                if (x >= (panel1.Width - pictureBox1.Width))
+                {
+                    x += 0;
+                }
+                else
+                {
+                    x += velocidade;
+                }
+
+            }
+            else if (e.KeyCode == Keys.Left) {
+                if(x <= 0)
+                {
+                    x -= 0;
+                }
+                else
+                {
+                    x -= velocidade;
+                }
+            }
+            else if (e.KeyCode == Keys.Up)
+            {
+                if (y <= 0)
+                {
+                    y -= 0;
+                }
+                else
+                {
+                    y -= velocidade;
+                }
+            }
+            else if (e.KeyCode == Keys.Down)
+            {
+                if (y >= (panel1.Height - pictureBox1.Height))
+                {
+                    y += 0;
+                }
+                else
+                {
+                    y += velocidade;
+                }
+            }
+
+            pictureBox1.Location = new Point(x, y);
+        }
+        int X = 0;
+        int Y = 0;
+        bool topo, esquerda;
+        void tm_Tick(object sender, EventArgs e)
+        {
+            int x = pictureBox2.Location.X;
+            int y = pictureBox2.Location.Y;
+            bool irParaEsquerda, irParaDireita, descer, subir;
+
+            X = ((int)(new Random().Next(-40 , -1)));
+            Y = ((int)(new Random().Next(-40, 40)));
+
+            if (x <= 0)
+            {
+                irParaEsquerda = false;
+                irParaDireita = true;
+                X = ((int)(new Random().Next(40)));
+            }
+            if(x >= panel1.Width)
+            {
+                irParaDireita = false;
+                irParaEsquerda = true;
+                X = ((int)(new Random().Next(-1, -40)));
+            }
+            if (y <= 0)
+            {
+                subir = false;
+                descer = true;
+                Y = ((int)(new Random().Next(1, 40)));
+            }
+            if (y >= panel1.Height)
+            {
+                descer = false;
+                subir = true;
+                Y = ((int)(new Random().Next(-1, -40)));
+            }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            pictureBox2.Location = new Point(X, Y);
+        }
+    }
+}
